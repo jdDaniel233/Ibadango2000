a4b916b00a427a6c384e887276970cb5a3bc2efa
Daniel <simbanadaniel42@gmail.com>
2023-05-25 16:23:45 -0500

    proyecto inicial

---
+++ app/src/main/java/com/example/ibadango2000/SiguienteActivity.java
@@ -0,0 +1,14 @@
+package com.example.ibadango2000;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SiguienteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_siguiente);
+    }
+}
